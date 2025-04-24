6c59736593d90b9405f1c95dd603a6f94fb89bad
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2025-04-24 12:14:04 +0530

    delete

--- src/components/common/FutureKonnect.tsx
+++
@@ -1,8 +0,0 @@
-import { Box, Typography } from '@mui/material';
-export default function FutureKonnect() {
-  return (
-    <Box>
-      <Typography variant="h6">FutureKonnect</Typography>
-    </Box>
-  );
-}
